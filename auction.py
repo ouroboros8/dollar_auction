95ed2b1a1a178edc0ea310fcbbb7d7a6e5720f03
Daniel Strong <dstrong@glyx.co.uk>
2015-02-22 18:26:24 +0000

    Add genome class, more restructuring.

--- auction.py
+++ auction.py
@@ -9,17 +9,23 @@ of a new, mutated population for the next round.
 import numpy
 import random
 
+from collections.abc import Sequence
+
 class Game():
     '''
     Object representing a run of the problem
     '''
 
-    def __init__(self, generations, pop_size, mem_len, gen_len):
-        self.mem_len = mem_len
-        self.gen_len = gen_len
+    def __init__(self, generations=1000, pop_size=100, genome_len=100,
+                 memory_len=100):
+        self.genome_len = genome_len
+        self.memory_len = memory_len
+
         self.pop_size = pop_size
         self.initial_population = self.get_initial_population()
         self.population = self.initial_population
+
+        self.generations = range(0, generations)
 
     def get_initial_population(self):
         '''
@@ -29,24 +35,9 @@ class Game():
         if self.initial_population:
             genomes = self.initial_population
         else:
-            genomes = [Individual(self.mem_len, self.random_genome())
+            genomes = [Genome(self.genome_len, self.memory_len)
                        for i in range(0, self.pop_size)]
         return genomes
-
-    def random_genome(self):
-        '''
-        Generate a random genome
-        '''
-        genome = [None]*self.gen_len
-        genome = [(random.choice(['i', 'd', 'e',
-                                  random.randint(0, self.mem_len)
-                                 ]
-                                ),
-                   random.randint(0, self.mem_len),
-                   (random.randint(0, self.gen_len),
-                    random.randint(0, self.gen_len))
-                  ) for elem in genome]
-        return genome
 
     def run(self):
         '''
@@ -57,7 +48,7 @@ class Game():
         '''
         Select the fittest members of a population.
         '''
-    
+
     def repopulate(self):
         '''
         Produce a new population from the fittest.
@@ -65,27 +56,77 @@ class Game():
 
 class Round():
     '''
-    Play a round of the game, using the given population and competition
-    structure (round robin by default).
+    A round of the game: tracks scores and creates matches.
     '''
 
     def __init__(self, population):
         self.population = population
-        self.pop_size = len(self.population)
 
     def round_robin(self):
         '''
         Each member of the current population plays a match against each
         other member. Too slow?
         '''
-        for i in range(0, self.pop_size-1):
-            for j in range(i+1, self.pop_size-1):
+        pop_size = len(self.population)
+        for i in range(0, pop_size-1):
+            for j in range(i+1, pop_size-1):
                 self.match(self.population[i], self.population[j])
 
     def match(self, ind1, ind2):
         '''
         Play the two individuals against each other.
         '''
+
+class Genome(Sequence):
+    '''
+    The genome consists of the "instruction set" of the individual, and
+    is a list of instructions, in the form of tuples. The first element
+    of each tuple denotes an operation, and the second the index in the
+    Individual's memory array to operate on. The possible tuples are as
+    follows:
+        ('i', n, (j, -)) increments the n^th integer in memory,
+                         then jumps to instruction j, - ignored;
+        ('d', n, (j, -)) decrements the n^th integer in memory, then
+                         jumps to instruction j, - ignored;
+        (n, m, (j, k))   jumps to instruction j if n >= m, or jumps to
+                         instruction k otherwise.
+        (e, -, (-, -)) end (all later vars irrelevant)
+        Obviously, n and m must be valid memory indices, and j and k
+        must be valid instruction set indices.
+    '''
+
+    def __init__(self, length, mem_len):
+        self.__list__ = [None]*length
+        self.__list__ = self.random_genome()
+        self.mem_len = mem_len
+
+    def __getitem__(self, i):
+        return self.__list__[i]
+
+    def __len__(self):
+        return len(self.__list__)
+
+    def __random_memory_index__(self):
+        return random.randint(0, self.mem_len-1)
+
+    def __random_genome_index__(self):
+        return random.randint(0, self.__len__-1)
+
+    def random_genome(self):
+        '''
+        Generate a random genome
+        '''
+        genome = [(random.choice(['i', 'd', 'e',
+                                  random.randint(
+                                      0,
+                                      self.__random_memory_index__()
+                                  )]
+                                ),
+                   random.randint(0, self.__random_memory_index__()),
+                   (random.randint(0, self.__random_genome_index__()),
+                    random.randint(0, self.__random_genome_index__()))
+                  ) for i in range(0, self.__random_genome_index__())]
+        return genome
 
 class Individual():
     '''
@@ -94,22 +135,7 @@ class Individual():
 
     def __init__(self, mem_len, genome):
         '''
-        Initialise an Individual with a certain genome. The genome
-        consists of the "instruction set" of the individual, and is a
-        list of instructions, in the form of tuples. The first element
-        of each tuple denotes an operation, and the second the index in
-        the Individual's memory array to operate on. The possible tuples
-        are as follows:
-            ('i', n, (j, -)) increments the n^th integer in memory, then
-                        jumps to instruction j, - ignored;
-            ('d', n, (j, -)) decrements the n^th integer in memory, then
-                        jumps to instruction j, - ignored;
-            (n, m, (j, k)) jumps to instruction j if n >= m, jumps to
-                        instruction k otherwise.
-            (e, -, (-, -)) end (all later vars irrelevant)
-        Obviously, n and m must be valid memory indices, and j and k
-        must be valid instruction set indices.
-        '''
+        Initialise an Individual with a certain genome.         '''
         self.genome = genome
         self.memory = numpy.array([0]*mem_len, dtype='uint')
         self.money = 0
@@ -121,7 +147,7 @@ class Individual():
         '''
         self.money = self.money + amount
 
-    def play(self, money, bids):
+    def play(self, bids):
         '''
         Make a bid, based on a list of previous bids, and amount of
         money left.
